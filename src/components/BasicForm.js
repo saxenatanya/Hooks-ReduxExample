69936b2202ce5262bf5c5938ed49d5a6841f28a7
Tanya Saxena <82470912+saxenatanya@users.noreply.github.com>
2021-09-11 13:00:07 -0400

    with validation in basic form

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,25 +1,91 @@
+import { useEffect, useState } from "react";
+import useInputBasic from "../hooks/use-inputBasic";
+
 const BasicForm = (props) => {
+  
+  const [formIsValid, setFormIsValid] = useState(false);
+  const {
+    value: enterName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError,
+    valueChangeHandler: nameChangeHandler,
+    reset: resetNameInput
+  } = useInputBasic(val => val.trim() !== '');
+
+  
+  const {
+    value: enterLName,
+    isValid: enteredLNameIsValid,
+    hasError: LnameInputHasError,
+    valueChangeHandler: LnameChangeHandler,
+    reset: resetLNameInput
+  } = useInputBasic(val => val.trim() !== '');
+
+  const {
+    value: enterEmail,
+    isValid: enteredEmailIsValid,
+    hasError: EmailInputHasError,
+    valueChangeHandler: emailChangeHandler,
+    reset: resetemailInput
+  } = useInputBasic(val => val.includes('@'));
+
+
+
+  useEffect(() => {
+    if (enteredNameIsValid &&enteredLNameIsValid&& enteredEmailIsValid) {
+      setFormIsValid(true);
+    } else {
+      setFormIsValid(false);
+    }
+  }, [enteredNameIsValid,enteredLNameIsValid,enteredEmailIsValid]);
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    // setEnteredNameTouched(true);
+    if (!enteredNameIsValid) {
+      return;
+    }
+    resetLNameInput('');
+    resetNameInput('');
+resetemailInput('');
+  };
+
+  
+  
+  const nameInputClasses = nameInputHasError ? 'form-control' : 'form-control invalid';
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='control-group'>
-        <div className='form-control'>
+        <div className={nameInputClasses}>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input type='text' id='name'
+            onChange={nameChangeHandler}
+            value={enterName}
+          />
+          {!nameInputHasError && <p>error</p>}
         </div>
-        <div className='form-control'>
+        <div className={nameInputClasses}>
           <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+          <input type='text' id='name'
+          value={enterLName}
+          onChange={LnameChangeHandler}/>
+        { !LnameInputHasError&& <p>error</p>}
         </div>
+
       </div>
       <div className='form-control'>
         <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' value={enterEmail}
+        onChange={emailChangeHandler}/>
+        { !EmailInputHasError&& <p>error</p>}
       </div>
       <div className='form-actions'>
-        <button>Submit</button>
+        <button disabled={!formIsValid} >Submit</button>
       </div>
     </form>
   );
 };
+
 
 export default BasicForm;
